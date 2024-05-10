508807620c5d1208fee7b5e2b12544dfe962a3b3
yadian <yadianfdez07@gmail.com>
2024-05-09 20:37:55 -0400

    fix powershell output and add complete env out

--- new-packerServicePrincipal.ps1
+++ new-packerServicePrincipal.ps1
@@ -1,4 +1,5 @@
 Write-Output "Create a new service principal for Packer";
 # $sp = az ad sp create-for-rbac --name "PackerServicePrincipal" --role Contributor --scopes /subscriptions/$subscriptionId
 
-Write-Output "Variable Value: $env:azSubscriptionId";
+Write-Output "Variable Value: $Env:azSubscriptionId";
+Get-ChildItem Env: | Format-Table -AutoSize
