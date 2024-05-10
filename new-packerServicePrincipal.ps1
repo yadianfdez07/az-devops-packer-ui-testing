1b7479c9b1006e18007c072c2a08cea0dd8bb2c1
yadian <yadianfdez07@gmail.com>
2024-05-10 08:08:13 -0400

    remove test lines for out put and create service principal in script

--- new-packerServicePrincipal.ps1
+++ new-packerServicePrincipal.ps1
@@ -1,6 +1,15 @@
 Write-Output "Create a new service principal for Packer";
 # $sp = az ad sp create-for-rbac --name "PackerServicePrincipal" --role Contributor --scopes /subscriptions/$subscriptionId
 
+
+$sp = New-AzADServicePrincipal -DisplayName "PackerPrincipal" -role Contributor -scope "/subscriptions/$Env:AZSUBSCRIPTIONID"
+
+$plainPassword = (New-AzADSpCredential -ObjectId $sp.Id).SecretText
+
 Write-Output "Variable Value: $Env:AZSUBSCRIPTIONID";
-Write-Output "Secret Variable Value: $Env:SECRETVAR";
 Get-ChildItem Env: | Format-Table -AutoSize
+
+$subName = "Personal-PAYG"
+$sub = Get-AzSubscription -SubscriptionName $subName
+
+Write-Output "Subscription Name: $sub";
