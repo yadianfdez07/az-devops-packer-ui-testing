30303de3308f9a4791b5291db85b1a5454405e0d
yadian <yadianfdez07@gmail.com>
2024-05-09 20:23:42 -0400

    add string to test output

--- new-packerServicePrincipal.ps1
+++ new-packerServicePrincipal.ps1
@@ -1,4 +1,4 @@
 Write-Output "Create a new service principal for Packer";
 # $sp = az ad sp create-for-rbac --name "PackerServicePrincipal" --role Contributor --scopes /subscriptions/$subscriptionId
 
-Write-Output $env:azSubscriptionId;
+Write-Output "Variable Value: $env:azSubscriptionId";
